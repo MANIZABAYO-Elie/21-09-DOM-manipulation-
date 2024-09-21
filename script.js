50dfa6401193deecf205f95f791c933d53ff7833
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-09-21 12:45:23 +0200

    file committed

---
+++ script.js
@@ -0,0 +1,5 @@
+const title = document.querySelector('#main_heading');
+title.style.color = 'green';
+console.log(title);
+const listItems = document.querySelectorAll('.list-item');
+console.log(listItems);
