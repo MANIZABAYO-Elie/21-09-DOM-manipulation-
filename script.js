cf36368de580311faa8fbb75b2a3ccda3b7dc089
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-09-22 22:19:50 +0200

    file committed

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const title = document.querySelector('#main_heading');
 title.style.color = 'green';
 console.log(title);
-const listItems = document.querySelectorAll('.list-item');
+//const listItems = document.querySelectorAll('.list-item');
 console.log(listItems);
-const list_item = document.querySelectorAll('class-item');
-list_item.style.color = 'gray';
-console.log(list_item);
+const heading = document.getElementsByClassName('h2');
+heading.style.color = 'green';
+console.log(heading);
