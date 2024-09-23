62e4a7a9bf4e850417341df9d4253092afaecb37
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-09-23 09:35:15 +0200

    file comitted

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const title = document.querySelector('#main_heading');
 title.style.color = 'green';
 console.log(title);
 //const listItems = document.querySelectorAll('.list-item');
-console.log(listItems);
-const heading = document.getElementsByTagName('h2');
-heading.style.color = 'green';
-console.log(heading);
+const btn = document.getElementById('btn');
+btn.style.background = 'orange'
+btn.style.color = 'white' ;
