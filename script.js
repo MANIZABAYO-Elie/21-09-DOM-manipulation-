eb26b4f0ecf37dc681450a9ccdf838685dd55b54
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-09-24 10:02:58 +0200

    file committed

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ console.log(title);
 //const listItems = document.querySelectorAll('.list-item');
 const btn = document.getElementById('btn');
 btn.style.background = 'orange'
-btn.style.color = 'white' ;
+btn.style.color = 'white';
+
+const items = document.getElementsByTagName('li');
+const betterItems = [...items];
+betterItems.forEach(function(item){
+    console.log(item);
+})
